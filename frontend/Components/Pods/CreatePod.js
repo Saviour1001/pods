6702e0f9ebd2b1bbd7e50199ef2d7b37f9b0aeec
Harsh Ghodkar <harshghodkar@gmail.com>
2022-07-19 18:13:09 +0530

    Update CreatePod.js

--- frontend/Components/Pods/CreatePod.js
+++ frontend/Components/Pods/CreatePod.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import {View, TextInput, TouchableOpacity} from 'react-native';
 import {H2, H3, styles} from '../shared/Typography';
 import {colors, fontFamilies, globalStyles} from '../shared/styles';
 import InputField from './Common/InputField';
-
+import {
+  useMoralisFile,
+  useMoralis,
+  useWeb3ExecuteFunction,
+} from "react-moralis";
 import Icon from '../shared/Icon';
 import iconNames from '../shared/iconNames';
 import HeaderWithBack from './Common/HeaderWithBack';
 import BottomButton from './Common/BottomButton';
+import ABI from "../../../smartContract/ABIs/podsV1.json";
+const contractAddress = "0x1977b8F6c789CbC7AF2bd066842F0fC47Fa583d3";
+
 
 const CreatePod = ({navigation}) => {
   const {goBack} = navigation;
@@ -20,6 +28,52 @@ const CreatePod = ({navigation}) => {
   const handelCreatePod = () => {
     console.log('Create new pod');
   };
+
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [files, setFiles] = useState([]);
+
+
+  const { saveFile } = useMoralisFile();
+  const { Moralis, account } = useMoralis();
+  const contractProcessor = useWeb3ExecuteFunction();
+
+  async function uploadFile(file) {
+    const imageHash = await saveFile("image",file,{saveIPFS:true}).then((res)=>{ console.log(res); return res; });
+  }
+
+  async function createPod() {
+    // call uploadFile for each file
+    const fileHashes = await Promise.all(files.map(async (file) => {
+      return await uploadFile(file);
+    }
+    ));
+    // call createPod
+    const options = {
+      contractAddress: contractAddress,
+      abi: ABI,
+      functionName: "createPod",
+      params: {
+        _contentUri: fileHashes,
+        _podMates: [account],
+      }
+    }
+
+    await contractProcessor.fetch({
+      params: options,
+    }).then(
+      (res) => {
+        console.log(res);
+      }
+    ).catch(
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+  
+
+
 
   const Input = ({label, action}) => {
     return (
