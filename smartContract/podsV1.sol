7c1beddafb8e99703c1fe19fb40b7be56203e73d
Harsh Ghodkar <harshghodkar@gmail.com>
2022-07-19 17:43:50 +0530

    adding smart contracts

---
+++ smartContract/podsV1.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+// deployed on BSC testnet: 0x1977b8F6c789CbC7AF2bd066842F0fC47Fa583d3
+pragma solidity ^0.8.7;
+
+contract PODS {
+    event PodCreated(
+        bytes32 indexed podId,
+        address indexed podOwner,
+        address[] podMates,
+        bytes32 contentId
+    );
+
+    struct Pod {
+        address owner;
+        bytes32 contentId;
+        address[] podMates;
+    }
+
+    mapping(bytes32 => Pod) podRegistry;
+
+    function createPod(
+        string calldata _contentUri,
+        address[] calldata _podMates
+    ) external {
+        address _owner = msg.sender;
+        bytes32 _contentId = keccak256(abi.encode(_contentUri));
+        bytes32 _podID = keccak256(
+            abi.encodePacked(_owner, block.timestamp, _contentId)
+        );
+
+        podRegistry[_podID].owner = _owner;
+        podRegistry[_podID].contentId = _contentId;
+        podRegistry[_podID].podMates = _podMates;
+        emit PodCreated(_podID, _owner, _podMates, _contentId);
+    }
+}
