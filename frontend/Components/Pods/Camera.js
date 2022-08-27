4c30175292c7ef9eb69ad3d004db0db9e07b75a2
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-27 18:23:07 +0530

    init

---
+++ frontend/Components/Pods/Camera.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Camera = () => {
+  return (
+    <View>
+      <Text>Camera</Text>
+    </View>
+  )
+}
+
+export default Camera
+
+const styles = StyleSheet.create({})
